2b2853db15fd44e1fdb489abc9dc79bd89781e82
Michał Stancelewski <michal@stancelewski.pl>
2023-08-30 11:38:09 +0200

    Mailer.

--- src/Controller/DomainsController.php
+++ src/Controller/DomainsController.php
@@ -7,6 +7,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -14,11 +16,11 @@ class DomainsController extends AbstractController
 {
     private const DELETED_DOMAINS_URL = 'https://www.dns.pl/deleted_domains.txt';
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, MailerInterface $mailer)
     {
         $this->entityManager = $entityManager;
+        $this->mailer = $mailer;
     }
-
 
     #[Route('/download-domains/', name: 'app_download_domains')]
     public function index(): JsonResponse
@@ -30,12 +32,13 @@ class DomainsController extends AbstractController
 
         $file = $this->getDomainsFromURL(DomainsController::DELETED_DOMAINS_URL);
 
-        foreach($file as $domain){
+        foreach ($file as $domain) {
             $domainsArray[] = new Domain($domain);
             $counter++;
         }
 
         $this->saveDomains($domainsArray);
+        $this->sendEmail($counter);
 
         return $this->json(
             "Downloaded " . $counter . " domains."
@@ -82,6 +85,46 @@ class DomainsController extends AbstractController
         } catch (\Exception $e) {
             $this->entityManager->rollback();
         }
+    }
+
+    public function sendEmail(int $count): void
+    {
+        /*
+                $emailAddress = $this->getParameter('ADMIN_EMAIL');
+                $emailSubject = 'Pobrano nowe domeny w liczbie '. $count . ' z listy usuniętych domen - Deleted Domains App';
+                $emailBody = '<p>Data: '. date('Y m d').'</p>'
+                    .
+                    '<br/>'
+                    .
+                    '<p>System <i>Deleted Domains App</i> pobrał z pliku tekstowego udostępnionego przez serwis DNS nowe domeny w liczbie: <b>'. $count .'</b>.</p>'
+                    .
+                    '<br/>'
+                    .
+                    '<p>Pobrane dzisiaj domeny zostaną zweryfikowane poprzez API za <b>'. $this->getParameter('DAYS_TO_VALIDATE') .'</b> dni.</p>'
+                    .
+                    '<br/>'
+                    .
+                    '<p>Otrzymasz w wiadomości email listę domen zweyfikowanych pozytywnie..</p>'
+                ;
+                $emailHeaders = [
+                    'From' => 'Deleted Domains App <noreply@mgmedia.pl>',
+                    'X-Mailer' => 'PHP/' . phpversion(),
+                    'MIME-Version' => '1.0',
+                    'Content-Type' => 'text/html; charset=iso-8859-1'
+                ];
+
+                mail($emailAddress, $emailSubject, $emailBody, $emailHeaders);
+        */
+
+        $email = (new Email())
+            ->from('noreply@mgmedia.pl')
+            ->to($this->getParameter('ADMIN_EMAIL'))
+            ->subject('Test')
+            ->html('Test!')
+        ;
+
+        $mailer = $this->mailer;
+        $mailer->send($email);
     }
 
 }
